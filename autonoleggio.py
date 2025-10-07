514b39651cc422f12ed2a2c0af5ba706ee72e480
noemi-dot <noemirinzo29@gmail.com>
2025-10-07 13:49:12 +0200

    prima modifica

--- autonoleggio.py
+++ autonoleggio.py
@@ -1,11 +1,34 @@
+class Automobile:
+    def __init__(self, codice, marca, modello, anno, posti):
+        self.codice = codice
+        self.marca = marca
+        self.modello = modello
+        self.anno = anno
+        self.posti = posti
+
 class Autonoleggio:
     def __init__(self, nome, responsabile):
         """Inizializza gli attributi e le strutture dati"""
-        # TODO
+        self.nome=nome
+        self.responsabile=responsabile
+        self.automobili=[]   #le caricheremo dal file automobili.csv
+
 
     def carica_file_automobili(self, file_path):
         """Carica le auto dal file"""
-        # TODO
+        try:
+            with open(file_path, "r", encoding="utf-8") as file:
+                for line in file:
+                    dati=line.strip().split(",")
+                    codice = dati[0]
+                    marca = dati[1]
+                    modello = dati[2]
+                    anno = int(dati[3])
+                    posti = int(dati[4]) if dati[4] else 0
+                    auto = Automobile(codice, marca, modello, anno, posti)
+                    self.automobili.append(auto)
+        except FileNotFoundError:
+            print("File non trovato")
 
     def aggiungi_automobile(self, marca, modello, anno, num_posti):
         """Aggiunge un'automobile nell'autonoleggio: aggiunge solo nel sistema e non aggiorna il file"""
